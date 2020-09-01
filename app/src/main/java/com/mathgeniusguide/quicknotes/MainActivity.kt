ef17ee36ea5b645fce8ba86ada4f74b8cc54cbaa
Jonathan Fitch <jfitch1@ymail.com>
2020-09-01 16:22:55 -0400

    added drawable resources, ran functionless app for first time

--- app/src/main/java/com/mathgeniusguide/quicknotes/MainActivity.kt
+++ app/src/main/java/com/mathgeniusguide/quicknotes/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.mathgeniusguide.quicknotes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        navController = findNavController(nav_host_fragment)
+        toolbar.setupWithNavController(navController)
+        tabs.setupWithNavController(navController)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        drawer_view.setNavigationItemSelectedListener {
+            it.onNavDestinationSelected(navController)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
